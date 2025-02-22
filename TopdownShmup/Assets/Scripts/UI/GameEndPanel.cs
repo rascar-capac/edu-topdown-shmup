8a5b15f811b6c0e8273f6d0eb3b7371747a869c5
Théo Constant <to.constant@gmail.com>
2025-02-22 20:32:02 +0100

    Add game end panel

---
+++ TopdownShmup/Assets/Scripts/UI/GameEndPanel.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class GameEndPanel : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup _canvasGroup;
+    [SerializeField] private TMP_Text _scoreText;
+    [SerializeField] private Button _retryButton;
+    [SerializeField] private Game _game;
+
+    private void Show()
+    {
+        _canvasGroup.alpha = 1f;
+        _scoreText.text = "no score";
+        _retryButton.onClick.AddListener(RetryButton_OnClick);
+    }
+
+    private void Hide()
+    {
+        _canvasGroup.alpha = 0f;
+    }
+
+    private void Game_OnGameEnded(bool gameIsWon)
+    {
+        Show();
+    }
+
+    private void RetryButton_OnClick()
+    {
+        Hide();
+    }
+
+    private void Awake()
+    {
+        Hide();
+        _game.OnGameEnded.AddListener(Game_OnGameEnded);
+    }
+}
