0c5f9b183dd760c5aa41755b8b9ef8d1b8f03461
Théo Constant <to.constant@gmail.com>
2025-02-23 19:20:03 +0100

    Use a tag list in target holder

--- TopdownShmup/Assets/Scripts/TargetHolder.cs
+++ TopdownShmup/Assets/Scripts/TargetHolder.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TargetHolder : MonoBehaviour
@@ -5,7 +6,7 @@ public class TargetHolder : MonoBehaviour
     [Tooltip("The target defaults to this object's transform.")]
     [SerializeField] private Transform _target;
     [Tooltip("If target is empty, this will be used to fetch a target (first one found). Leave empty to use the object's transform instead.")]
-    [SerializeField] private string _tag;
+    [SerializeField] private List<string> _tagList;
 
     private Transform _actualTarget;
 
@@ -15,7 +16,7 @@ public class TargetHolder : MonoBehaviour
         {
             // lazy initialization: if the target must be fetched with a tag, it only gets initialized when we actually call Target.
             // this way, it removes the overhead from the scene initialization and ensures that the object will be looked for until found, without searching for it every frame
-            if (_target == null && (string.IsNullOrEmpty(_tag) || !FindTransformWithTag(_tag, out _actualTarget)))
+            if (_target == null && !TryFindTransformWithAnyTag(_tagList, out _actualTarget))
             {
                 _actualTarget = transform;
             }
@@ -28,7 +29,47 @@ public class TargetHolder : MonoBehaviour
         }
     }
 
-    public static bool FindGameObjectWithTag(string tag, out GameObject gameObject)
+    private static bool TryFindGameObjectWithAnyTag(List<string> tags, out GameObject gameObject)
+    {
+        foreach (string tag in tags)
+        {
+            if (string.IsNullOrEmpty(tag))
+            {
+                continue;
+            }
+
+            if (TryFindGameObjectWithTag(tag, out gameObject))
+            {
+                return true;
+            }
+        }
+
+        gameObject = null;
+
+        return false;
+    }
+
+    private static bool TryFindTransformWithAnyTag(List<string> tags, out Transform transform)
+    {
+        foreach (string tag in tags)
+        {
+            if (string.IsNullOrEmpty(tag))
+            {
+                continue;
+            }
+
+            if (TryFindTransformWithTag(tag, out transform))
+            {
+                return true;
+            }
+        }
+
+        transform = null;
+
+        return false;
+    }
+
+    public static bool TryFindGameObjectWithTag(string tag, out GameObject gameObject)
     {
         try
         {
@@ -44,9 +85,9 @@ public class TargetHolder : MonoBehaviour
         }
     }
 
-    public static bool FindTransformWithTag(string tag, out Transform transform)
+    public static bool TryFindTransformWithTag(string tag, out Transform transform)
     {
-        if (FindGameObjectWithTag(tag, out GameObject gameObject))
+        if (TryFindGameObjectWithTag(tag, out GameObject gameObject))
         {
             transform = gameObject.transform;
 
