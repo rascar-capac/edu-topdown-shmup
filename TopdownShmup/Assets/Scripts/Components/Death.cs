b599c35780802959f866e56236962e5ac9aeffe9
Théo Constant <to.constant@gmail.com>
2025-02-24 23:47:04 +0100

    Remove enemy health asset and replace by exposed float values + trigger enemy death with exposed event

--- TopdownShmup/Assets/Scripts/Components/Death.cs
+++ TopdownShmup/Assets/Scripts/Components/Death.cs
@@ -1,11 +1,8 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-[RequireComponent(typeof(Health))]
 public class Death : MonoBehaviour
 {
-    [SerializeField] private Health _health;
-
     [SerializeField] private UnityEvent _onTriggered = new();
 
     public UnityEvent OnTriggered => _onTriggered;
@@ -14,28 +11,5 @@ public class Death : MonoBehaviour
     {
         Destroy(gameObject);
         _onTriggered.Invoke();
-    }
-
-    private void TrackingValue_OnValueChanged(float newValue, float ratio)
-    {
-        if (ratio == 0f)
-        {
-            Trigger();
-        }
-    }
-
-    private void Awake()
-    {
-        if (_health == null)
-        {
-            Debug.LogWarning("No Health provided.", this);
-
-            return;
-        }
-
-        if (_health.Data != null)
-        {
-            _health.Data.OnValueChanged.AddListener(TrackingValue_OnValueChanged);
-        }
     }
 }
--- TopdownShmup/Assets/Scripts/Components/Health.cs
+++ TopdownShmup/Assets/Scripts/Components/Health.cs
@@ -3,52 +3,63 @@ using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] private FloatInRange _data;
-    [SerializeField] private bool _usesRangeAsClone;
+    [Tooltip("If empty, float values below will be used.")]
+    [SerializeField] private FloatInRange _healthAsset;
+    [SerializeField] private float _initialHealth;
+    [SerializeField] private float _minHealth;
+    [SerializeField] private float _maxHealth;
 
-    [SerializeField] private UnityEvent _onHurt = new();
-    [SerializeField] private UnityEvent _onHealed = new();
+    [SerializeField] private UnityEvent<float> _onHurt = new();
+    [SerializeField] private UnityEvent<float> _onHealed = new();
+    [SerializeField] private UnityEvent _onMinReached = new();
 
-    private FloatInRange _dataClone;
+    private float _health;
 
-    public FloatInRange Data
-    {
-        get
-        {
-            if (_usesRangeAsClone)
-            {
-                if (_dataClone == null && _data != null)
-                {
-                    _dataClone = _data.Clone();
-                }
-
-                return _dataClone;
-            }
-
-            return _data;
-        }
-    }
-
-    public UnityEvent OnHurt => _onHurt;
-    public UnityEvent OnHealed => _onHealed;
+    public UnityEvent<float> OnHurt => _onHurt;
+    public UnityEvent<float> OnHealed => _onHealed;
+    public UnityEvent OnMinReached => _onMinReached;
 
     public void Hurt(float damagePercentage)
     {
         ModifyHealth(-damagePercentage);
-        _onHurt.Invoke();
+        _onHurt.Invoke(GetRatio());
     }
 
     public void Heal(float healPercentage)
     {
         ModifyHealth(healPercentage);
-        _onHealed.Invoke();
+        _onHealed.Invoke(GetRatio());
     }
 
     private void ModifyHealth(float healthToAdd)
     {
-        if (Data != null)
+        if (_healthAsset != null)
         {
-            Data.Value += healthToAdd;
+            _healthAsset.Value += healthToAdd;
+
+            if (_healthAsset.Value == _healthAsset.MinValue)
+            {
+                _onMinReached.Invoke();
+            }
         }
+        else
+        {
+            _health = Mathf.Clamp(_health + healthToAdd, _minHealth, _maxHealth);
+
+            if (_health == _minHealth)
+            {
+                _onMinReached.Invoke();
+            }
+        }
+    }
+
+    private float GetRatio()
+    {
+        return _healthAsset != null ? _healthAsset.CurrentRatio : Mathf.InverseLerp(_minHealth, _maxHealth, _health);
+    }
+
+    private void Awake()
+    {
+        _health = _initialHealth;
     }
 }
